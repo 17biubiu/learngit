b5590a765bf3cc1cf1af7d0e04525ba77630c80e
lishuai <lishuai@zuoyebang.com>
2019-11-08 16:28:15 +0800

    feat: 应该不分叉吧

---
+++ test.js
@@ -0,0 +1,15 @@
+class Shenme {
+  constructor () {
+    this.why = why
+    this.result = result
+  }
+
+  handleWhy () {
+    let sss = this.why
+    console.log(sss)
+  }
+
+  handleResult () {
+    console.log(this.result)
+  }
+}
